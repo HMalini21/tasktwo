7c67edac9a1bf3e483c5a44836c40019c18f4348
AMalini21 <hemaarumugam19@gmail.com>
2024-07-07 18:17:22 +0530

    using event listerner

--- task--two/README.md
+++ task--two/README.md
@@ -1,2 +1,5 @@
 # Using Props and state
 in this project we are using state as props by creating a separate components and then adding the props to it
+
+// now we are adding an event listener in this project
+by click it to change the color to its opposite color
--- task--two/src/components/Square.js
+++ task--two/src/components/Square.js
@@ -1,7 +1,15 @@
+import React from 'react';
+
 export default function Square(props) {
+  const [on, setOn] = React.useState(props.on);
   const style = {
-    backgroundColor: props.on ? '#cccccc' : '#222222',
+    backgroundColor: on ? '#cccccc' : '#222222',
   };
+
+  function toggle() {
+    setOn((prevOn) => !prevOn);
+  }
+
   console.log(props.on);
-  return <div className="box" style={style}></div>;
+  return <div className="box" style={style} onClick={toggle}></div>;
 }
